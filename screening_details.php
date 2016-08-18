96c3e2c71daab0da453a0324da689f8a3706a719
Sally <sally@thecreativetemp.com>
2016-08-18 15:53:36 -0600

    Returns fields to template so I can get a look at what's going on.

--- screening_details.php
+++ screening_details.php
@@ -1,3 +1,18 @@
-<?php get_header();?>
-MY SCREENING
+<?php get_header();
+$value = get_metadata( 'screening_details', get_the_ID()); // Returns post metadata value for the field 'featured'
+$postMeta = get_post_meta(get_the_ID());
+?>
+
+
+
+<?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>    
+        <?php the_title();?>
+        <pre>
+        <?php print_r($postMeta);?>
+    </pre>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+
 <?php get_footer();?>
