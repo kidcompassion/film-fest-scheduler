7268320a7bb5adcbd03c652227dceb6beb359dac
Sally <sally@thecreativetemp.com>
2016-08-19 12:09:25 -0600

    Adds table for grid.

--- screening_archive.php
+++ screening_archive.php
@@ -1,9 +1,4 @@
-<?php get_header();
-
-
-
-
-?>
+<?php get_header();?>
 
 <?php 
 // the query
@@ -23,14 +18,56 @@ $the_query = new WP_Query( $args );
     <!-- the loop -->
     <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-    <pre>          
-        <?php
+    <?php 
+    $filmRuntime = get_post_meta(get_the_ID(),'fs_run_time'); // Returns post metadata value for the field 'featured'
+    $filmDescription = get_post_meta(get_the_ID(),'fs_filmdescription');
+    $filmTitle = get_post_meta(get_the_ID(),'fs_title');
+    $filmTime = get_post_meta(get_the_ID(),'fs_screening_time');
+    $filmDate = get_post_meta(get_the_ID(),'fs_screening_date');
+    $filmLocation = get_post_meta(get_the_ID(),'fs_screening_location');
+    $filmYear = get_post_meta(get_the_ID(),'fs_production_year');
+    $filmDirector = get_post_meta(get_the_ID(),'fs_director');
+    $filmWebsite = get_post_meta(get_the_ID(),'fs_film_website');
+    $filmTrailer = get_post_meta(get_the_ID(),'fs_trailer');
+    $filmCountry = get_post_meta(get_the_ID(),'fs_country');
+    $filmTickets = get_post_meta(get_the_ID(),'fs_tickets');
 
 
-           print_r(get_post_meta(get_the_ID()));
-        ?>
-</pre>
+    ?>
 
+
+<table>
+<tbody>
+    <tr>
+        <td>
+
+        <?php if (isset($filmTime[0])):?>
+            <p><?php echo $filmTime[0];?></p>
+        <?php endif;?>
+
+        </td>
+
+        <td>
+
+        <?php if (isset($filmDate[0])):?>
+            <p><?php echo $filmDate[0];?></p>
+        <?php endif;?>
+
+        </td>
+        <td>
+
+        <?php if (isset($filmTitle[0])):?>
+            <p><strong>Title:</strong><?php echo $filmTitle[0];?></p>
+        <?php endif;?>
+
+        </td>
+
+    </tr>
+
+
+</tbody>
+
+</table>
 
 
     <?php endwhile; ?>
